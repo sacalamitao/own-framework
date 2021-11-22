12a40505a470dd947fe08accb018d953bf75762c
edwin sacalamitao <sacalamitao.edwin@yahoo.com.ph>
2021-11-22 12:36:42 +0800

    update

--- this_is_for_js_or_jquery.js
+++ this_is_for_js_or_jquery.js
@@ -1,3 +1,4 @@
+
   $(document).ready(function () {
     var searchInput = "search_input"    
     loadGooglePlaces((document.getElementById(searchInput)), {
@@ -11,4 +12,41 @@
         types: ['geocode'],
     });
   });
+
 //---------- google api this is for ID, if a new item will be generated need to change it to class
+
+  $(document).on 'keyup', '.numbersOnly', ->
+    if @value != @value.replace(/[^0-9\.]*$/g, '')
+    @value = @value.replace(/[^0-9\.]*$/g, '')
+    return
+//--------------------REGEX for numbers and dollar sign only
+
+
+$(document).on 'keyup', '.numbersOnly', ->
+  if @value != @value.replace(/[^0-9\.]/g, '')
+    @value = @value.replace(/[^0-9\.]/g, '')
+  return
+
+$(document).on 'keyup', '.numbersOnly', ->
+  if @value != @value.replace(/\.(?=.*\.)/g, '')
+    @value = @value.replace(/\.(?=.*\.)/g, '')
+  return 
+
+
+
+// --------------------REGEX for one DOT only------------------
+var total = document.querySelector("#testId");
+  $(document).on('click', total, function(){
+    // var total = document.querySelector(".total");
+    console.log('test total')
+
+  });
+
+  // var testest = document.querySelector(".numbersonly");
+  // $(document).on('keyup', testest, function(){ 
+  //   console.log('test')
+
+  // });​
+
+
+// ---------------------------------------
